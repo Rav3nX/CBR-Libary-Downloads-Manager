4c72c98bd41e483c271064967df8a687b5a70685
Rav3nX <Raviena500@gmail.com>
2017-04-11 23:03:37 +1000

    Added the libary browser

--- CBR Libary Checker/LibaryViewer.Designer.vb
+++ CBR Libary Checker/LibaryViewer.Designer.vb
@@ -26,8 +26,9 @@ Partial Class LibaryViewer
         Me.ToolStrip2 = New System.Windows.Forms.ToolStrip()
         Me.LibaryPlane = New System.Windows.Forms.FlowLayoutPanel()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.ComicInfoDB1 = New CBR_Libary_Checker.ComicInfoDB()
         Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ComicInfoDB1 = New CBR_Libary_Checker.ComicInfoDB()
         Me.Panel1.SuspendLayout()
         CType(Me.ComicInfoDB1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -36,16 +37,16 @@ Partial Class LibaryViewer
         '
         Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(895, 25)
+        Me.ToolStrip1.Size = New System.Drawing.Size(908, 25)
         Me.ToolStrip1.TabIndex = 0
         Me.ToolStrip1.Text = "ToolStrip1"
         '
         'ToolStrip2
         '
         Me.ToolStrip2.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.ToolStrip2.Location = New System.Drawing.Point(0, 505)
+        Me.ToolStrip2.Location = New System.Drawing.Point(0, 455)
         Me.ToolStrip2.Name = "ToolStrip2"
-        Me.ToolStrip2.Size = New System.Drawing.Size(895, 25)
+        Me.ToolStrip2.Size = New System.Drawing.Size(908, 25)
         Me.ToolStrip2.TabIndex = 1
         Me.ToolStrip2.Text = "ToolStrip2"
         '
@@ -56,38 +57,47 @@ Partial Class LibaryViewer
         Me.LibaryPlane.Dock = System.Windows.Forms.DockStyle.Fill
         Me.LibaryPlane.Location = New System.Drawing.Point(246, 25)
         Me.LibaryPlane.Name = "LibaryPlane"
-        Me.LibaryPlane.Size = New System.Drawing.Size(649, 480)
+        Me.LibaryPlane.Size = New System.Drawing.Size(662, 430)
         Me.LibaryPlane.TabIndex = 2
         '
         'Panel1
         '
+        Me.Panel1.Controls.Add(Me.ListView1)
         Me.Panel1.Controls.Add(Me.TreeView1)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
         Me.Panel1.Location = New System.Drawing.Point(0, 25)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(246, 480)
+        Me.Panel1.Size = New System.Drawing.Size(246, 430)
         Me.Panel1.TabIndex = 3
-        '
-        'ComicInfoDB1
-        '
-        Me.ComicInfoDB1.DataSetName = "ComicInfoDB"
-        Me.ComicInfoDB1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'TreeView1
         '
         Me.TreeView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.TreeView1.Location = New System.Drawing.Point(3, 78)
+        Me.TreeView1.Location = New System.Drawing.Point(3, 86)
         Me.TreeView1.Name = "TreeView1"
-        Me.TreeView1.Size = New System.Drawing.Size(237, 399)
-        Me.TreeView1.TabIndex = 0
+        Me.TreeView1.Size = New System.Drawing.Size(237, 341)
+        Me.TreeView1.TabIndex = 2
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(0, 3)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(237, 79)
+        Me.ListView1.TabIndex = 1
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'ComicInfoDB1
+        '
+        Me.ComicInfoDB1.DataSetName = "ComicInfoDB"
+        Me.ComicInfoDB1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'LibaryViewer
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(895, 530)
+        Me.ClientSize = New System.Drawing.Size(908, 480)
         Me.Controls.Add(Me.LibaryPlane)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.ToolStrip2)
@@ -106,5 +116,6 @@ Partial Class LibaryViewer
     Friend WithEvents LibaryPlane As FlowLayoutPanel
     Friend WithEvents Panel1 As Panel
     Friend WithEvents ComicInfoDB1 As ComicInfoDB
+    Friend WithEvents ListView1 As ListView
     Friend WithEvents TreeView1 As TreeView
 End Class
--- CBR Libary Checker/LibaryViewer.vb
+++ CBR Libary Checker/LibaryViewer.vb
@@ -1,14 +1,16 @@
 ﻿Imports System.IO
 
 Public Class LibaryViewer
-    Private Sub LibaryViewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        For Each Dir As String In Directory.GetDirectories(My.Settings.LibaryFolder)
-            Dim newnode As New TreeNode
-            newnode.Name = Dir
-            newnode.Text = Dir
-            TreeView1.Nodes.Add(newnode)
 
-        Next
+    Public Sub New()
+        InitializeComponent()
+        PopulateTreeView()
+
+    End Sub 'New
+
+    Private Sub LibaryViewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
 
         For i As Integer = 1 To 100
             LibaryPlane.Controls.Add(New LibaryItem)
@@ -16,7 +18,89 @@ Public Class LibaryViewer
         Next
     End Sub
 
-    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
+    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs)
+
+    End Sub
+    Private Sub PopulateTreeView()
+        Dim rootNode As TreeNode
+
+        Dim info As New DirectoryInfo(My.Settings.LibaryFolder)
+        If info.Exists Then
+            rootNode = New TreeNode(info.Name)
+            rootNode.Tag = info
+            GetDirectories(info.GetDirectories(), rootNode)
+            TreeView1.Nodes.Add(rootNode)
+        End If
+
+    End Sub
+
+    Private Sub GetDirectories(ByVal subDirs() As DirectoryInfo,
+        ByVal nodeToAddTo As TreeNode)
+
+        Dim aNode As TreeNode
+        Dim subSubDirs() As DirectoryInfo
+        Dim subDir As DirectoryInfo
+        For Each subDir In subDirs
+            Try
+                aNode = New TreeNode(subDir.Name, 0, 0)
+                aNode.Tag = subDir
+                aNode.ImageKey = "folder"
+                subSubDirs = subDir.GetDirectories()
+                If subSubDirs.Length <> 0 Then
+                    GetDirectories(subSubDirs, aNode)
+                End If
+                nodeToAddTo.Nodes.Add(aNode)
+            Catch ex As Exception
+            End Try
+
+        Next subDir
+
+    End Sub
+    Private Sub treeView1_NodeMouseClick(ByVal sender As Object, ByVal e As TreeNodeMouseClickEventArgs) Handles TreeView1.NodeMouseClick
+
+        Dim newSelected As TreeNode = e.Node
+        ListView1.Items.Clear()
+        Dim nodeDirInfo As DirectoryInfo = CType(newSelected.Tag, DirectoryInfo)
+        Dim subItems() As ListViewItem.ListViewSubItem
+        Dim item As ListViewItem = Nothing
+
+        Dim dir As DirectoryInfo
+        For Each dir In nodeDirInfo.GetDirectories()
+            item = New ListViewItem(dir.Name, 0)
+            subItems = New ListViewItem.ListViewSubItem() _
+                {New ListViewItem.ListViewSubItem(item, "Directory"),
+                New ListViewItem.ListViewSubItem(item,
+                dir.LastAccessTime.ToShortDateString())}
+
+            item.SubItems.AddRange(subItems)
+            ListView1.Items.Add(item)
+        Next dir
+
+        Dim file As FileInfo
+        For Each file In nodeDirInfo.GetFiles() ' List view items
+            item = New ListViewItem(file.Name, 1)
+            subItems = New ListViewItem.ListViewSubItem() _
+                {New ListViewItem.ListViewSubItem(item, "File"),
+                New ListViewItem.ListViewSubItem(item,
+                file.LastAccessTime.ToShortDateString())}
+
+            item.SubItems.AddRange(subItems)
+            ListView1.Items.Add(item)
+        Next file
+        LibaryPlane.Controls.Clear()
+
+        For Each file In nodeDirInfo.GetFiles() ' comic info items
+            Dim new_item As New LibaryItem
+            new_item.Label1.Text = file.Name
+            LibaryPlane.Controls.Add(new_item)
+
+        Next file
+
+        ListView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize)
+
+    End Sub
+
+    Private Sub TreeView1_AfterSelect_1(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
 
     End Sub
 End Class
